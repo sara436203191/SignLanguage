f29a12dac8430ff9c3711becde51f9c68632938a
sara436203191 <s-k-a9@hotmail.com>
2016-04-30 20:54:02 +0300

    add 4 and 9

--- Learning/Form4.cs
+++ Learning/Form4.cs
@@ -9,9 +9,9 @@ using System.Windows.Forms;
 
 namespace Learning
 {
-    public partial class Form6 : Form
+    public partial class Form4 : Form
     {
-        public Form6()
+        public Form4()
         {
             InitializeComponent();
             pictureBox9.Parent = pictureBox1;
@@ -20,35 +20,19 @@ namespace Learning
             pictureBox10.BackColor = Color.Transparent;
             pictureBox11.Parent = pictureBox1;
             pictureBox11.BackColor = Color.Transparent;
-            pictureBox5.Parent = pictureBox1;
-            pictureBox5.BackColor = Color.Transparent;
-            label1.Parent = pictureBox1;
-            label1.BackColor = Color.Transparent;
-
-        }
-
-    /*    private void pictureBox3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }*/
-
-        private void Form6_Load(object sender, EventArgs e)
-        {
-            label1.Text = "أهلا و سهلا بك يا " + Class1.name;
+           label1.Parent = pictureBox1;
+           label1.BackColor = Color.Transparent;
+           label2.Parent = pictureBox1;
+           label2.BackColor = Color.Transparent;
+           label3.Parent = pictureBox1;
+           label3.BackColor = Color.Transparent;
         }
 
         private void pictureBox9_Click(object sender, EventArgs e)
         {
-            Form2 f2 = new Form2();
-            f2.Show();
+            Form6 f6 = new Form6();
+            f6.Show();
             this.Hide();
-
         }
 
         private void pictureBox10_Click(object sender, EventArgs e)
@@ -56,28 +40,50 @@ namespace Learning
             Form2 f2 = new Form2();
             f2.Show();
             this.Hide();
+
         }
 
         private void pictureBox11_Click(object sender, EventArgs e)
         {
             Application.Exit();
+
         }
 
-      /*  private void pictureBox1_Click(object sender, EventArgs e)
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            label1.Text = "أهلا و سهلا بك يا " + Class1.name;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
         {
 
         }
-        */
-      
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Form9 f9 = new Form9();
+            f9.Show();
+            this.Hide();
+        }
 
         private void pictureBox5_Click(object sender, EventArgs e)
         {
-            Form4 f4 = new Form4();
-            f4.Show();
+            Form14 f14 = new Form14();
+            f14.Show();
             this.Hide();
-
         }
-        
-      
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            Form15 f15 = new Form15();
+            f15.Show();
+            this.Hide();
+        }
     }
+}
+
+class Game
+{
+    public static int numOfc = 0;
+    public static int numOfw = 0;
 }
